5532543c991035f2e22d4f4914cfc1b4fcfbfc2b
eidicaro <icaroeid@gmail.com>
2025-02-25 14:27:05 -0300

    Add do pacote do boostrap

--- Tong-TCC/App.js
+++ Tong-TCC/App.js
@@ -1,12 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Stack = createNativeStackNavigator();
+
+function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Bem-vindo ao seu aplicativo!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#000', // Altera o fundo da nav-bar para preto
+          },
+          headerTintColor: '#fff', // Altera a cor do texto da nav-bar para branco
+        }}
+      >
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
