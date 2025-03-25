ac7a3b828a9012a9a857486820ad3f3a875cee48
eidicaro <icaroeid@gmail.com>
2025-03-25 14:31:26 -0300

    Arrumamo a home :0

--- tong_tcc/src/App.js
+++ tong_tcc/src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './paginas/header.js'; // ajuste o caminho conforme a estrutura do seu projeto
 import Footer from './paginas/footer.js';
 import Carrossel from './paginas/carrossel.js';
+import Btt from './paginas/btt-cardapio.js';
 
 const App = () => {
   return (
@@ -14,7 +15,17 @@ const App = () => {
               <h1>Seja Bem-Vindo!</h1>
               <p>Somos a Tong Sushi,  uma casa de culinária japonesa localizada em Iperó-SP,  agora com uma cara nova e um novo sistema de delivery para sua melhor experiência.</p>
             </main>  
-            < Carrossel/>   
+            < Carrossel/>  
+
+            {/* Horário de Atendimento */}
+            <div className='horario'>
+            <h1>Horário de Atendimento</h1>
+            
+              <p>Segunda-Quinta: 18:30-22:00</p>
+              <p>Sexta-sabado: 18:30-22:30</p>
+            </div>
+            <Btt/>
+            
             <Footer/>
       </div>
       </html>
--- tong_tcc/src/paginas/carrossel.js
+++ tong_tcc/src/paginas/carrossel.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import Tong from './../images/tong-2.svg';
-import sashimi from './../images/sashimi-salmao.png'
 
 const Carrossel = () => {
   return (
